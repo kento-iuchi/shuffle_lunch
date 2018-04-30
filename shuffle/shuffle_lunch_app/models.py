2fb0bd4029d6885f473ce26ef3a7c968ce9d7a34
kento-iuchi <31495048+kento-iuchi@users.noreply.github.com>
2018-04-30 12:17:30 +0900

    memberテーブルにcan_go追加

--- shuffle/shuffle_lunch_app/models.py
+++ shuffle/shuffle_lunch_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Member(models.Model):
     name = models.CharField(max_length=200)
+    can_go = models.BooleanField(default = True)
 
     def __str__(self):
         return self.name
