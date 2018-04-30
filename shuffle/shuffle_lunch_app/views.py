fe7b197d984d99460075c719b4339f4316ee0f9f
kento-iuchi <31495048+kento-iuchi@users.noreply.github.com>
2018-04-30 12:38:03 +0900

    メンバー表示

--- shuffle/shuffle_lunch_app/views.py
+++ shuffle/shuffle_lunch_app/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Member
+
 def teams(request):
-    return render(request, 'shuffle_lunch_app/teams.html', {})
+    members = Member.objects.all()
+    return render(request, 'shuffle_lunch_app/teams.html', {'members' : members})
 # Create your views here.
