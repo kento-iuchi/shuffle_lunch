adf5dc2d3ff1628c33056eba6f30d08bfbbb1e33
kento-iuchi <31495048+kento-iuchi@users.noreply.github.com>
2018-05-03 21:54:55 +0900

    view/organize_teamsへの非同期通信実装

--- shuffle/shuffle_lunch_app/static/shuffle_lunch_app/teams.js
+++ shuffle/shuffle_lunch_app/static/shuffle_lunch_app/teams.js
@@ -7,7 +7,21 @@
     class ShuffleButton extends React.Component{
         render(){
             return (
-                <form id="organize_team" action="organize/" onSubmit={function(){ alert('clicked'); }}>
+                // action="organize/"
+                <form id="organize_team" onSubmit={
+                    function(){
+                        var current_url = location.href;
+                        console.log(current_url);
+                        fetch(current_url + 'organize/', {
+                            method: 'POST',
+                            body: 'test'
+                        })
+                        .then(response => {
+                            console.log(response);
+                        })
+                        alert('clicked');
+                    }}
+                >
                 <button type="submit">
                     shuffle!
                 </button>
--- shuffle/shuffle_lunch_app/views.py
+++ shuffle/shuffle_lunch_app/views.py
@@ -5,6 +5,10 @@ def teams(request):
     members = Member.objects.all()
     return render(request, 'shuffle_lunch_app/teams.html', {'members' : members})
 
-def organize_team(request):
-    return 0s
+def organize_teams(request):
+    import json
+    from django.http import HttpResponse,Http404
+
+    response = json.dumps({'your_surprise_txt':'aaa',})
+    return HttpResponse(response, content_type = "text/javascript")
 # Create your views here.
