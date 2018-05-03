397f1303cdbe3580e9e97cd782d0147ebe02e396
kento-iuchi <31495048+kento-iuchi@users.noreply.github.com>
2018-05-03 11:28:35 +0900

    id設定

--- shuffle/shuffle_lunch_app/models.py
+++ shuffle/shuffle_lunch_app/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 class Member(models.Model):
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=200)
     can_go = models.BooleanField(default = True)
 
