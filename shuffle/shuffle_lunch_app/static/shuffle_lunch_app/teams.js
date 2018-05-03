b6473a20067befc25941fc3bafdd25963bab3126
kento-iuchi <31495048+kento-iuchi@users.noreply.github.com>
2018-05-03 21:25:30 +0900

    jsにorganize実装

--- shuffle/shuffle_lunch_app/static/shuffle_lunch_app/teams.js
+++ shuffle/shuffle_lunch_app/static/shuffle_lunch_app/teams.js
@@ -1,1 +1,23 @@
 // aaa
+// import CommentBox from './comment_box.js.jsx';
+(() => {
+
+    // import { Component } from 'react';
+
+    class ShuffleButton extends React.Component{
+        render(){
+            return (
+                <form id="organize_team" action="organize/" onSubmit={function(){ alert('clicked'); }}>
+                <button type="submit">
+                    shuffle!
+                </button>
+                </form>
+            )
+        }
+    }
+
+    ReactDOM.render(
+        <ShuffleButton />,
+        document.getElementById('shuffle-button')
+    );
+})();
--- shuffle/shuffle_lunch_app/views.py
+++ shuffle/shuffle_lunch_app/views.py
@@ -4,4 +4,7 @@ from .models import Member
 def teams(request):
     members = Member.objects.all()
     return render(request, 'shuffle_lunch_app/teams.html', {'members' : members})
+
+def organize_team(request):
+    return 0s
 # Create your views here.
